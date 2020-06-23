52d6c3a068fd570adfd72ee31aeeb334545d72a5
zhiling1995 <3353004045@qq.com>
2020-06-23 14:03:51 +0800

    first commit

--- src/api/user.js
+++ src/api/user.js
@@ -1,24 +1,25 @@
 import request from '@/utils/request'
 
 export function login(data) {
-  return request({
-    url: '/vue-admin-template/user/login',
-    method: 'post',
-    data
-  })
+    console.log(data)
+    return request({
+        url: '/api/login',
+        method: 'post',
+        data
+    })
 }
 
 export function getInfo(token) {
-  return request({
-    url: '/vue-admin-template/user/info',
-    method: 'get',
-    params: { token }
-  })
+    return request({
+        url: '/api/user',
+        method: 'get',
+        params: { token }
+    })
 }
 
 export function logout() {
-  return request({
-    url: '/vue-admin-template/user/logout',
-    method: 'post'
-  })
+    return request({
+        url: '/api/logout',
+        method: 'post'
+    })
 }
